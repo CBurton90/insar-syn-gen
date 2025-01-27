58c2d0b4547a697d94f5eadc4f0cb2fe902777d8
CBurton90 <conradburton90@gmail.com>
2025-01-27 20:50:23 +1300

    added matplotlib subplots for velocities

--- variogram/spatial_variogram.py
+++ variogram/spatial_variogram.py
@@ -1,20 +1,30 @@
 import h5py as h5
 import numpy as np
+import matplotlib.pyplot as plt
 import skgstat as skg
 from scipy.optimize import curve_fit
 
-file = '096A_13547_NZ_GNS_rural.h5'
+file = '044D_12520_NZ_GNS_hres.h5'
+subset = False
 
 def construct_variogram(filepath):
-	f = h5.File(filepath, 'r')
-	datasetNames = [n for n in f.keys()]
-	for n in datasetNames:
-		print(n)
-		print(type(f[n]))
-	vel = f['Velocity']
-	rows, cols = vel.shape[0], vel.shape[1]
+    f = h5.File(filepath, 'r')
+    datasetNames = [n for n in f.keys()]
+    for n in datasetNames:
+        print(n)
+        print(type(f[n]))
+    vel = f['Velocity']
+    vel_masked = np.ma.masked_array(vel, mask=np.isnan(vel))
+    rows, cols = vel.shape[0], vel.shape[1]
+    print(f'array rows and cols are {rows},{cols}')
 
-	#if rows > cols:
+    center_row = rows // 2
+    center_col = cols // 2
+    start_idx = -500
+    end_idx = 0
+    count_idx = 500
+
+    #if rows > cols:
 		#rand_rows = np.random.randint(0, rows, round(0.8*cols,))
 		#rand_cols = np.random.randint(0, cols, round(0.8*cols,))
 	#else:
@@ -26,42 +36,74 @@ def construct_variogram(filepath):
 	#rand_cols = np.random.randint(0, cols, (round(0.01*(rows*cols)),))
 		
 	#coords = np.column_stack((rand_rows,rand_cols))
-
-	if rows > cols:
-		coords = np.random.randint(0, 2000, (round(0.02*(2000**2)),2))
-	else:
-		coords = np.random.randint(0, 2000, (round(0.02*(2000**2)),2))
-	print(coords.shape)
+    if subset:
+        if rows > cols:
+            coords = np.random.randint(center_col+start_idx, center_col+end_idx, (round(0.6*(count_idx**2)),2))
+        else:
+            coords = np.random.randint(center_row+start_idx, center_row+end_idx, (round(0.6*(count_idx**2)),2))
+        print(coords.shape)
+    else:
+        if rows > cols:
+            x = np.linspace(center_col+start_idx, center_col+end_idx, count_idx, dtype=int)
+            y = np.linspace(center_col+start_idx, center_col+end_idx, count_idx, dtype=int)
+            yy, xx = np.meshgrid(y, x)
+            coords = np.column_stack((yy.flatten(), xx.flatten()))
+        else:
+            x = np.linspace(center_row+start_idx, center_row+end_idx, count_idx, dtype=int)
+            y = np.linspace(center_row+start_idx, center_row+end_idx, count_idx, dtype=int)
+            yy, xx = np.meshgrid(y, x)
+            coords = np.column_stack((yy.flatten(), xx.flatten()))
+        print(coords.shape)
 	
 	# only use values/coords where non-NaN
-	mask = ~np.isnan(np.fromiter((vel[c[0], c[1]] for c in coords), dtype=float))
-	values = np.fromiter((vel[c[0], c[1]] for c in coords), dtype=float)
-	masked_vals = values[mask]
+    mask = ~np.isnan(np.fromiter((vel[c[0], c[1]] for c in coords), dtype=float))
+    values = np.fromiter((vel[c[0], c[1]] for c in coords), dtype=float)
+    masked_vals = values[mask]
 	#masked_rows = rand_rows[mask]
 	#masked_cols = rand_cols[mask]
 	#masked_coords = np.column_stack((masked_rows,masked_cols))
-	masked_coords = coords[mask]
-	print(values)
-	print(values.shape)
-	print(masked_vals)
-	print(masked_vals.shape)
-	print(masked_coords.shape)
+    masked_coords = coords[mask]
+    print(values)
+    print(values.shape)
+    print(masked_vals)
+    print(masked_vals.shape)
+    print(masked_coords.shape)
 
-	# assert value in original array equal masked array
-	#assert vel[masked_rows[0], masked_cols[0]] == masked_vals[0]
+    lat = f['Latitude']
+    print('masking lat')
+    #lat_masked = np.ma.masked_array(lat, mask=np.isnan(lat))
+    long = f['Longitude']
+    print('masking long')
+    #long_masked = np.ma.masked_array(long, mask=np.isnan(long))
+    print('grouping lats')
+    lats_grouped = np.fromiter((lat[c[0], c[1]] for c in masked_coords), dtype=float)
+    print('grouping longs')
+    longs_grouped = np.fromiter((long[c[0], c[1]] for c in masked_coords), dtype=float)
 
-	V = skg.Variogram(masked_coords, masked_vals, n_lags=25, bin_func='even')
-	V.fit_method ='lm'
-	fig = V.plot(show=False)
-	fig.savefig('test_variogram_'+file+'.png')
-	fig2 = V.distance_difference_plot()
-	fig2.savefig('test_dist_diff_'+file+'.png')
+    # assert value in original array equal masked array
+    #assert vel[masked_rows[0], masked_cols[0]] == masked_vals[0]
+    print('calculating variogram')
+    V = skg.Variogram(masked_coords, masked_vals, n_lags=30, bin_func='uniform')
+    #V.fit_method ='lm'
+    #fig = V.plot(show=False)
+    #fig.savefig('test_variogram_'+file+'.png')
+    #fig2 = V.distance_difference_plot()
+    #fig2.savefig('test_dist_diff_'+file+'.png')
 
-	print(V.parameters)
+    fig, ax = plt.subplots(2, 2, figsize=(15,10))
+    print('plotting full frame')
+    ax[0,0].scatter(long[::50, ::50], lat[::50, ::50], c=vel[::50, ::50], cmap='bwr_r', s=3)
+    #ax[0,0] = plt.scatter(long_masked[::100, ::100], lat_masked[::100, ::100], c=vel_masked[::100, ::100], cmap='bwr_r')
+    print('plotting frame subset')
+    ax[0,1].scatter(longs_grouped, lats_grouped, c=masked_vals, cmap='bwr_r', s=3)
+    #ax[1,0].plot(V.bins, V.experimental, '.b')
+    V.plot(axes=ax[1,0], show=False)
+    V.distance_difference_plot(ax=ax[1,1], show=False)
+    fig.savefig('inspect_variogram_'+file+'.png')
 
-	
-	
-	# set estimator back
+    print(V.parameters)
+
+    # set estimator back
 	#V.estimator = 'matheron'
 	#V.model = 'spherical'
 	#xdata = V.bins
